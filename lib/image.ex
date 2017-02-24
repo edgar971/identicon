c71d6f7835dc1079d821b6a0a950189efe83b8a6
Edgar Pino <me@edgarpino.com>
2017-02-23 18:03:48 -0600

    added additional function to generate images

--- lib/image.ex
+++ lib/image.ex
@@ -1,9 +1,10 @@
 defmodule Identicon.Image do 
+    
     @moduledoc """
     Documentation for Identicon.Image.
     """
 
-    defstruct hex: nil, color: nil
+    defstruct hex: nil, color: nil, grid: nil, pixel_map: nil
 
 
 
